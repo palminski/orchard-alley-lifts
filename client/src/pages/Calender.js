39b266d1e5032bc126c4ceb27b4d103e106768c0
palminski <williambolls98@gmail.com>
2023-03-07 10:11:47 -0800

    calender can be updated from front end

--- client/src/pages/Calender.js
+++ client/src/pages/Calender.js
@@ -1,24 +1,24 @@
 import { useQuery, useMutation } from "@apollo/client";
 import {QUERY_CURRENT_USER} from '../utils/queries';
+import { EDIT_CALENDER } from "../utils/mutations";
 import {useState} from 'react';
 
 const Calender = () => {
 
-    //===[States]=============================================
-    const [calenderState, setCalenderState] = useState({
-        monday: "none",
-        tuesday: "none",
-        wednesday: "none",
-        thursday: "none",
-        friday: "none",
-        saturday: "none",
-        sunday: "none",
-    })
+    
 
     //===[Queries]=============================================
     const { loading, data, refetch } = useQuery(QUERY_CURRENT_USER);
     const user = (data?.currentUser)
-    console.log(data);
+    console.log(user.calender);
+
+    //===[Mutations]=============================================
+    const [editCalender] = useMutation(EDIT_CALENDER);
+
+    
+    //===[States]=============================================
+    const [calenderState, setCalenderState] = useState(user.calender);
+    
 
     //===[Functions]=============================================
     const handleFormChange = (event) => {
@@ -33,9 +33,19 @@ const Calender = () => {
         console.log("Hello World.");
     };
 
-    const handleFormSubmit = (event) => {
+    async function handleFormSubmit(event) {
         event.preventDefault();
         console.log(calenderState);
+        try {
+            const mutationResponse = await editCalender({
+                variables: calenderState
+            });
+            refetch();
+        }
+        catch (error) {
+            console.log(error);
+        }
+
 
     };
 
@@ -44,7 +54,7 @@ const Calender = () => {
         <h1>Calender</h1>
             <form onSubmit={handleFormSubmit}>
             <label htmlFor="monday">Monday: </label>
-                <select name="monday" onChange={handleFormChange}>
+                <select name="monday" value={calenderState.monday} onChange={handleFormChange}>
                     <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
@@ -52,48 +62,48 @@ const Calender = () => {
                 </select>
                 <br></br>
                 <label htmlFor="tuesday">Tuesday: </label>
-                <select name="tuesday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="tuesday" value={calenderState.tuesday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
                 </select>
                 <br></br>
                 <label htmlFor="wednesday">Wednesday: </label>
-                <select name="wednesday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="wednesday" value={calenderState.wednesday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
                 </select>
                 <br></br>
                 <label htmlFor="thursday">Thursday: </label>
-                <select name="thursday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="thursday" value={calenderState.thursday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
                 </select>
                 <br></br>
                 <label htmlFor="friday">Friday: </label>
-                <select name="friday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="friday" value={calenderState.friday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
                 </select>
                 <br></br>
                 <label htmlFor="saturday">Saturday: </label>
-                <select name="saturday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="saturday" value={calenderState.saturday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
                 </select>
                 <br></br>
                 <label htmlFor="sunday">Sunday: </label>
-                <select name="sunday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                <select name="sunday" value={calenderState.sunday} onChange={handleFormChange}>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
