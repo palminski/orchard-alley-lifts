2e73028766e0b275daab9c09ac8168ea5c1c1e47
palminski <williambolls98@gmail.com>
2023-03-07 09:24:02 -0800

    calender can collect input from user!

--- client/src/pages/Calender.js
+++ client/src/pages/Calender.js
@@ -1,7 +1,19 @@
 import { useQuery, useMutation } from "@apollo/client";
 import {QUERY_CURRENT_USER} from '../utils/queries';
+import {useState} from 'react';
 
 const Calender = () => {
+
+    //===[States]=============================================
+    const [calenderState, setCalenderState] = useState({
+        monday: "none",
+        tuesday: "none",
+        wednesday: "none",
+        thursday: "none",
+        friday: "none",
+        saturday: "none",
+        sunday: "none",
+    })
 
     //===[Queries]=============================================
     const { loading, data, refetch } = useQuery(QUERY_CURRENT_USER);
@@ -9,17 +21,31 @@ const Calender = () => {
     console.log(data);
 
     //===[Functions]=============================================
-    const handleFormChange = () => {
+    const handleFormChange = (event) => {
+        const {name, value} = event.target; 
+
+        setCalenderState({
+            ...calenderState,
+            [name]:value
+        })
+        console.log(calenderState);
+
         console.log("Hello World.");
-    }
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(calenderState);
+
+    };
 
     return (
         <>
         <h1>Calender</h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
             <label htmlFor="monday">Monday: </label>
                 <select name="monday" onChange={handleFormChange}>
-                    <option value="none"></option>
+                    <option value="none">none</option>
                     {user.workouts && user.workouts.map(workout => (
                         <option key={workout._id} value={workout._id}>{workout.name}</option>
                     ))}
