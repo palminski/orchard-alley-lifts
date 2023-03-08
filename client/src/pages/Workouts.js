e61a0d8af843ba522b44fba4d779ada35e1b4914
palminski <williambolls98@gmail.com>
2023-03-08 09:12:59 -0800

    improved workout page functionality

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -149,7 +149,7 @@ const Workouts = () => {
                                         <button onClick={() => setMode("add")}>New Workout</button>
                                         <br></br>
                                         <label htmlFor="workouts">Select a Workout: </label>
-                                        <select name="workouts" onChange={handleSelectChange}>
+                                        <select name="workouts" onChange={handleSelectChange} value={user.workouts[selectedWorkoutIndex]?._id}>
                                             <option value="none"></option>
                                             {user.workouts && user.workouts.map(workout => (
                                                 <option key={workout._id} value={workout._id}>{workout.name}</option>
@@ -234,14 +234,14 @@ const Workouts = () => {
                                     :
                                     <>
                                         <button onClick={() => setMode("select")}>Select a Workout</button>
-                                        <AddWorkoutForm></AddWorkoutForm>
+                                        <AddWorkoutForm setMode={setMode} setSelectedWorkoutIndex={setSelectedWorkoutIndex}></AddWorkoutForm>
                                     </>
                                 }
                             </>
                             :
                             <>
                                 <h2>Add your first workout!</h2>
-                                <AddWorkoutForm></AddWorkoutForm>
+                                <AddWorkoutForm setMode={setMode} setSelectedWorkoutIndex={setSelectedWorkoutIndex}></AddWorkoutForm>
                             </>
                         }
                     </>
