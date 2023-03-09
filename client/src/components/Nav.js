3a67bfa71eba27927cf930439749d1b728a5433c
deand925 <97762768+deand925@users.noreply.github.com>
2023-03-08 18:30:45 -0800

    added styling to login and homepage

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,51 +5,67 @@ const Nav = (props) => {
   const { pageSelected, setPageSelected } = props;
   const client = useApolloClient();
   return (
-    <nav className="nav-container">
-      <ul>
-        {Auth.loggedIn() && (
-          <>
-            <a>
-              <li href="#Workouts" onClick={() => setPageSelected("Workouts")}>
-                Workouts
-              </li>
-            </a>
-            <a>
-              <li href="#Today" onClick={() => setPageSelected("Today")}>
-                Today
-              </li>
-            </a>
-            <a>
-              <li href="#Calender" onClick={() => setPageSelected("Calender")}>
-                Calender
-              </li>
-            </a>
-            <div className="logSignupLinks">
-            <a>
-            {!Auth.loggedIn() && 
-            <a>
-                <li href='#Login' onClick={() => setPageSelected('Login')}>Login</li>
-            </a>}
-            {!Auth.loggedIn() && 
-            <a>
-                <li href='#SignUp' onClick={() => setPageSelected('Signup')}>Sign Up</li>
-            </a>}
-              <li
-                href="/"
-                onClick={() => {
-                  Auth.logout();
-                  client.clearStore();
-                  setPageSelected("Home");
-                }}
-              >
-                Logout
-              </li>
-            </a>
-            </div>
-          </>
-        )}
-      </ul>
-    </nav>
+    <>
+      <nav className="nav-container">
+        <h1>Example Title</h1>
+        <div className="hrefCon">
+            <h2>Logo</h2>
+          <ul>
+            {!Auth.loggedIn() && (
+              <a>
+                <li href="#Login" onClick={() => setPageSelected("Login")}>
+                  Login
+                </li>
+              </a>
+            )}
+            {!Auth.loggedIn() && (
+              <a>
+                <li href="#SignUp" onClick={() => setPageSelected("Signup")}>
+                  Sign Up
+                </li>
+              </a>
+            )}
+            {Auth.loggedIn() && (
+              <>
+                <a>
+                  <li
+                    href="#Workouts"
+                    onClick={() => setPageSelected("Workouts")}
+                  >
+                    Workouts
+                  </li>
+                </a>
+                <a>
+                  <li href="#Today" onClick={() => setPageSelected("Today")}>
+                    Today
+                  </li>
+                </a>
+                <a>
+                  <li
+                    href="#Calender"
+                    onClick={() => setPageSelected("Calender")}
+                  >
+                    Calender
+                  </li>
+                </a>
+                <a>
+                  <li
+                    href="/"
+                    onClick={() => {
+                      Auth.logout();
+                      client.clearStore();
+                      setPageSelected("Home");
+                    }}
+                  >
+                    Logout
+                  </li>
+                </a>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
