d8afe23b51017213833f1a80f4bde575554c356f
palminski <williambolls98@gmail.com>
2023-03-20 11:17:14 -0700

    set up client set a bit diferently to prepare for offline queue

--- client/src/utils/getApolloClient/client.js
+++ client/src/utils/getApolloClient/client.js
@@ -1,22 +1,81 @@
-import { InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
+import { InMemoryCache, ApolloClient, createHttpLink, ApolloLink} from '@apollo/client';
 import { CachePersistor } from 'apollo3-cache-persist';
+import {RetryLink} from 'apollo-link-retry';
+import QueueLink from 'apollo-link-queue';
+import {onError} from 'apollo-link-error';
+import SerializingLink from 'apollo-link-serialize';
+
 import { setContext } from '@apollo/client/link/context'
 const API_HOST = 'http://localhost:3001/graphql';
 const SCHEMA_VERSION = '1';
 const SCHEMA_VERSION_KEY = 'apollo-schema-version';
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
-});
+
 
 const getApolloClient = async () => {
     const httpLink = new createHttpLink({ uri: API_HOST })
+    const retryLink = new RetryLink({attempts: {max:Infinity}})
+
+    const authLink = setContext((_, { headers }) => {
+        const token = localStorage.getItem('id_token');
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : '',
+            },
+        };
+    });
+
+    const errorLink = onError(({ networkError}) => {
+        if (networkError && networkError.statusCode === 401) {
+            localStorage.removeItem('id_token');
+            window.location.replace('/login');
+        }
+    })
+
+    const queueLink = new QueueLink();
+    window.addEventListener("offline", () => queueLink.close());
+    window.addEventListener('online', () => queueLink.open());
+
+    const serializingLink = new SerializingLink();
+
+    const trackerLink = new ApolloLink((operation, forward) => {
+        if (forward === undefined) return null;
+
+        const context = operation.getContext();
+        const trackedQueries = JSON.parse(window.localStorage.getItem('trackedQueries') || null) || [];
+
+        if (context.tracked !== undefined) {
+            const {operationName, query, variables} = operation;
+
+            const newTrackedQuery = {
+                query,
+                context,
+                variables,
+                operationName,
+            }
+
+            window.localStorage.setItem('trackedQueries', JSON.stringify([...trackedQueries, newTrackedQuery]))
+        }
+        return forward(operation).map((data) => {
+            if (context.tracked !== undefined) {
+                window.localStorage.setItem('trackedQueries', JSON.stringify(trackedQueries))
+            }
+            return data
+        })
+    })
+
+
+    const link = ApolloLink.from([
+        trackerLink,
+        queueLink,
+        serializingLink,
+        retryLink,
+        errorLink,
+        authLink,
+        httpLink
+    ])
+
     const cache = new InMemoryCache()
 
     const persistor = new CachePersistor({
@@ -33,7 +92,15 @@ const getApolloClient = async () => {
         window.localStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION)
     }
 
-    return new ApolloClient({ link: authLink.concat(httpLink), cache })
+    //I think we can make client this way
+    // return new ApolloClient({ link: authLink.concat(httpLink), cache })
+
+    const client = new ApolloClient({
+        link,
+        cache,
+    })
+
+    return client
 }
 
 export default getApolloClient;
