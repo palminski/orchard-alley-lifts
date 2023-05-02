7aeb9f56246fa5d8d9f80857cb2b78bd3863caf7
palminski <williambolls98@gmail.com>
2023-05-02 14:22:33 -0700

    delete exercises persists

--- client/src/utils/updateFunctions/index.js
+++ client/src/utils/updateFunctions/index.js
@@ -1,10 +1,12 @@
 import AddExercise from "./addExercise";
+import DeleteExercise from "./DeleteExercise";
 import AddWorkout from "./AddWorkout";
 import DeleteWorkout from "./DeleteWorkout";
 import EditWorkout from "./EditWorkout";
 
 export{
     AddExercise,
+    DeleteExercise,
     AddWorkout,
     DeleteWorkout,
     EditWorkout,
